17eb0fc42ebd959a76952186a93b5843735864d8
Harishguhan <harishguhan@dorustree.in>
2022-11-17 20:23:29 +0530

    Form Design Completeted

--- Forms/SignUpForm.js
+++ Forms/SignUpForm.js
@@ -1,13 +1,61 @@
-import { View } from "react-native";
+import { useState } from "react";
+import { Button, View } from "react-native";
 import Input from "../components/Input";
 
-export default function SignupForm(){
-    return(
-        <View>
-            <Input label="Username" placeholder="Enter Your name"/>
-            <Input label="Email" placeholder="Enter Your Email"/>
-            <Input label="Password" placeholder="Enter Your password"/>
-            <Input label="Address" placeholder="Enter Your Address"/>
-        </View>
-    );
+export default function SignupForm() {
+  const [inputValues, setInputvalues] = useState({
+    username: "",
+    email: "",
+    password: "",
+    address: "",
+  });
+  const handleChange = (inputidentifier, enteredText) => {
+    setInputvalues((currentInput) => {
+      return {
+        ...currentInput,
+        [inputidentifier]: enteredText,
+      };
+    });
+  };
+  const handleSubmit = () => {
+    console.log(inputValues);
+  }
+  return (
+    <View>
+      <Input
+        label="Username"
+        textInputConfig={{
+          placeholder: "Enter your name",
+          onChangeText: handleChange.bind(this, "username"),
+          value:inputValues.username
+        }}
+        
+      />
+      <Input
+        label="Email"
+        textInputConfig={{
+          placeholder: "Enter your Email",
+          onChangeText: handleChange.bind(this, "email"),
+          value:inputValues.email
+        }}
+      />
+      <Input
+        label="Password"
+        textInputConfig={{
+          placeholder: "Enter your Password",
+          onChangeText: handleChange.bind(this, "password"),
+          value:inputValues.password
+        }}
+      />
+      <Input
+        label="Address"
+        textInputConfig={{
+          placeholder: "Enter your Address",
+          onChangeText: handleChange.bind(this, "address"),
+          value:inputValues.address
+        }}
+      />
+      <Button title="Submit" onPress={handleSubmit} /> 
+    </View>
+  );
 }
--- components/Input.js
+++ components/Input.js
@@ -1,11 +1,29 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import { TextInput } from "react-native-gesture-handler";
 
-export default function Input({label,placeholder}){
+export default function Input({label,textInputConfig}){
     return(
-        <View>
-            <Text>{label}</Text>
-            <TextInput placeholder={placeholder} />
+        <View style={styles.inputContainer}>
+            <Text style={styles.label}>{label}</Text>
+            <TextInput style={styles.input} {...textInputConfig} />
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    inputContainer:{
+        marginHorizontal:8,
+        marginVertical:16,
+    },
+    label:{
+        fontSize:12,
+        marginBottom:10,
+    },
+    input:{
+        backgroundColor:'white',
+        padding:8,
+        fontSize:18,
+        borderRadius:8
+    }
+
+});
